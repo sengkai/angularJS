57a257f933d49ac0453989654a7e71779c7dbbd0
skylover <extinct.world.87@gmail.com>
2017-06-07 17:10:49 +0800

    - Create service class that is sharable by many other components - ngOnInit lifecycle hook to get data when AppComponent is active - HeroService is define as a provider for AppComponent - Design a service that return a Promoise - Component to get data from Promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import {Hero} from "./hero";
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { Hero } from "./hero";
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
@@ -80,13 +68,26 @@ const HEROES: Hero[] = [
     </ul>
     <hero-detail [hero]="selectedHero"></hero-detail>
   `,
+  providers: [HeroService]
 })
-export class AppComponent {
-  title = 'Basic AngularJS';
-  heroes = HEROES;
+export class AppComponent implements OnInit {
+  title = 'Basic AngularJS [Heros] ';
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { };
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   };
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 };
+
+
