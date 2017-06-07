14778c5623e9142bc951e3107fece4d3c0daf324
skylover <extinct.world.87@gmail.com>
2017-06-07 16:28:43 +0800

    - Refactor to unload burden on app.component - Created a new class hero detail to handle all hero stuff - Refactor to allow the component to be more reusable and loose coupling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-  id: number;
-  name: string;
-};
+import {Hero} from "./hero";
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 @Component({
   selector: 'app-root',
@@ -57,46 +67,26 @@ export class Hero {
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
-,
+  `],
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-    <h2> List of Heros </h2>
+    <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heros">
-        <!-- each hero goes here-->
+      <li *ngFor="let hero of heroes"
+          [class.selected]="hero === selectedHero"
+          (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-  `
+    <hero-detail [hero]="selectedHero"></hero-detail>
+  `,
 })
 export class AppComponent {
   title = 'Basic AngularJS';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm fly'
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   };
-  heros = HEROES;
 };
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
-
-
